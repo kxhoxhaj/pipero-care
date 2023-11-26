14562c7a44ed6fe1f47b3f84894073c4f1da95d5
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2023-11-27 00:38:27 +0100

    created serives index routes,  controller,  view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :services, only: [:index]
+  resources :packages, only: %i[index show]
+  resources :tests, only: [:index]
+  resources :dermoesthetics, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
