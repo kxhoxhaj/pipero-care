ee8026160c0c5db1d968f5f95543b73f54a8848d
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2023-12-06 21:33:02 +0100

    added icon, updated gem file completed rails admin

--- Gemfile
+++ Gemfile
@@ -47,7 +47,7 @@ gem "bootsnap", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-
+gem "cloudinary"
 gem "bootstrap", "~> 5.2"
 gem "devise"
 gem "rails_admin", "3.0"
@@ -79,4 +79,3 @@ group :test do
   gem "selenium-webdriver"
 
 end
-gem "sassc-rails"
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,16 @@
 RailsAdmin.config do |config|
   config.asset_source = :sprockets
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
+  config.authorize_with do
+    unless current_user.admin?
+      flash[:alert] = 'Sorry, no admin access for you.'
+      redirect_to main_app.root_path
+    end
+  end
 
   ### Popular gems integration
 
